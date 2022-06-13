9219928fc3f777b5732c5eb7b1f82c8fbdd2cab0
Half04 <half04@yandex.ru>
2022-06-14 01:51:13 +0300

    (+) Домашнее задание 11: вариант с WeakReference-ами.

--- hw11-cache/src/main/java/ru/otus/cachehw/HWCacheDemo.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/HWCacheDemo.java
@@ -7,26 +7,49 @@ import org.slf4j.LoggerFactory;
 public class HWCacheDemo {
     private static final Logger logger = LoggerFactory.getLogger(HWCacheDemo.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new HWCacheDemo().demo();
     }
 
-    private void demo() {
+    private void demo() throws InterruptedException {
         HwCache<String, Integer> cache = new MyCache<>();
 
         // пример, когда Idea предлагает упростить код, при этом может появиться "спец"-эффект
-        HwListener<String, Integer> listener = new HwListener<String, Integer>() {
+        HwListener<String, Integer> listener1 = new HwListener<String, Integer>() {
             @Override
             public void notify(String key, Integer value, String action) {
                 logger.info("key:{}, value:{}, action: {}", key, value, action);
             }
         };
 
-        cache.addListener(listener);
+        HwListener<String, Integer> listener2 = new HwListener<String, Integer>() {
+            @Override
+            public void notify(String key, Integer value, String action) {
+                logger.info("key2:{}, value2:{}, action2: {}", key, value, action);
+            }
+        };
+
+        HwListener<String, Integer> listener3 = new HwListener<String, Integer>() {
+            @Override
+            public void notify(String key, Integer value, String action) {
+                logger.info("key3:{}, value3:{}, action3: {}", key, value, action);
+            }
+        };
+
+        cache.addListener(listener1);
+        cache.addListener(listener2);
+        cache.addListener(listener3);
         cache.put("1", 1);
 
         logger.info("getValue:{}", cache.get("1"));
         cache.remove("1");
-        cache.removeListener(listener);
+
+        listener2 = null;
+        listener1 = null;
+        listener3 = null;
+
+        System.gc();
+
+        Thread.sleep(200);
     }
 }
--- hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,18 +1,30 @@
 package ru.otus.cachehw;
 
 
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MyCache<K, V> implements HwCache<K, V> {
+    private static final Logger logger = LoggerFactory.getLogger(MyCache.class);
     private static final String PUT = "put";
     private static final String REMOVE = "remove";
     private static final String GET = "get";
 
     private final Map<K, V> cache = new WeakHashMap<>();
-    private final List<HwListener<K, V>> listeners = new ArrayList<>();
+    private final ReferenceQueue<HwListener<K, V>> referenceQueue = new ReferenceQueue<>();
+    private final List<WeakReference<HwListener<K, V>>> listeners = new ArrayList<>();
+
+    public MyCache() {
+        start();
+    }
 
     @Override
     public void put(K key, V value) {
@@ -42,16 +54,34 @@ public class MyCache<K, V> implements HwCache<K, V> {
     @Override
     public void addListener(HwListener<K, V> listener) {
         if (listener != null) {
-            listeners.add(listener);
+            listeners.add(new WeakReference<>(listener, referenceQueue));
         }
+
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        listeners.remove(listener);
+//        listeners.remove(listener);
     }
 
     private void notify(K key, V value, String action) {
-        listeners.forEach(listener -> listener.notify(key, value, action));
+        listeners.forEach(listener -> listener.get().notify(key, value, action));
+    }
+
+    private void start() {
+        new Thread(
+            () -> {
+                while (true) {
+                    try {
+                        Reference<? extends HwListener<K, V>> removed = referenceQueue.remove();
+                        listeners.remove(removed);
+                        logger.info("Object cleaned:{}", removed);
+
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        ).start();
     }
 }
