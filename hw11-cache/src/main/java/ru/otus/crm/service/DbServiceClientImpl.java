bd7822cfe2dc3eb7d2f9401ede3989455f4b818f
Half04 <half04@yandex.ru>
2022-06-13 23:48:53 +0300

    (+) Домашнее задание 11.

---
+++ hw11-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -0,0 +1,65 @@
+package ru.otus.crm.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.cachehw.HwCache;
+import ru.otus.cachehw.MyCache;
+import ru.otus.core.repository.DataTemplate;
+import ru.otus.core.sessionmanager.TransactionRunner;
+import ru.otus.crm.model.Client;
+
+public class DbServiceClientImpl implements DBServiceClient {
+    private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
+
+    private final DataTemplate<Client> dataTemplate;
+    private final TransactionRunner transactionRunner;
+    private final HwCache<Long, Client> cache = new MyCache<>();
+
+    public DbServiceClientImpl(TransactionRunner transactionRunner, DataTemplate<Client> dataTemplate) {
+        this.transactionRunner = transactionRunner;
+        this.dataTemplate = dataTemplate;
+    }
+
+    @Override
+    public Client saveClient(Client client) {
+        return transactionRunner.doInTransaction(connection -> {
+            if (client.getId() == null) {
+                var clientId = dataTemplate.insert(connection, client);
+                var createdClient = new Client(clientId, client.getName());
+                log.info("created client: {}", createdClient);
+                cache.put(clientId, createdClient);
+                return createdClient;
+            }
+            dataTemplate.update(connection, client);
+            log.info("updated client: {}", client);
+            cache.put(client.getId(), client);
+            return client;
+        });
+    }
+
+    @Override
+    public Optional<Client> getClient(long id) {
+        var clientFromCache = cache.get(id);
+        if (clientFromCache != null) {
+            return Optional.of(clientFromCache);
+        }
+        return transactionRunner.doInTransaction(connection -> {
+            var clientOptional = dataTemplate.findById(connection, id);
+            log.info("client: {}", clientOptional);
+            clientOptional.ifPresent(client -> cache.put(client.getId(), client));
+            return clientOptional;
+        });
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return transactionRunner.doInTransaction(connection -> {
+            var clientList = dataTemplate.findAll(connection);
+            log.info("clientList:{}", clientList);
+            return clientList;
+       });
+    }
+}
