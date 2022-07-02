71f049de8576a12a6acf5cdf9ec6fc676b32cd5f
Half04 <half04@yandex.ru>
2022-07-03 00:09:55 +0300

    (+) Домашнее задание 16.

---
+++ hw16-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -0,0 +1,57 @@
+package ru.otus.services.processors;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.lib.SensorDataBufferedWriter;
+import ru.otus.api.SensorDataProcessor;
+import ru.otus.api.model.SensorData;
+
+// Этот класс нужно реализовать
+public class SensorDataProcessorBuffered implements SensorDataProcessor {
+    private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
+
+    private final int bufferSize;
+    private final SensorDataBufferedWriter writer;
+    private final BlockingQueue<SensorData> dataBuffer;
+
+    public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
+        this.bufferSize = bufferSize;
+        this.writer = writer;
+        dataBuffer = new PriorityBlockingQueue<>(bufferSize, Comparator.comparing(SensorData::getMeasurementTime));
+    }
+
+    @Override
+    public void process(SensorData data) {
+        try {
+            dataBuffer.put(data);
+            if (dataBuffer.size() >= bufferSize) {
+                flush();
+            }
+        } catch (Exception e) {
+            log.error("Ошибка в процессе обработки показаний датчика", e);
+        }
+    }
+
+    public void flush() {
+        try {
+            var bufferedData = new ArrayList<SensorData>();
+            dataBuffer.drainTo(bufferedData);
+            if (!bufferedData.isEmpty()) {
+                writer.writeBufferedData(bufferedData);
+            }
+        } catch (Exception e) {
+            log.error("Ошибка в процессе записи буфера", e);
+        }
+    }
+
+    @Override
+    public void onProcessingEnd() {
+        flush();
+    }
+}
